ba0bc99f924861aa1fd85756cae00211f2274f78
KnowOne <Knowone134@gmail.com>
2023-05-16 17:51:09 -0500

    database data

---
+++ npclogger/tacocat's data/database/Eastern Altepa Desert.lua
@@ -0,0 +1,6 @@
+local zone_database =
+{
+    [17244656] = {['id']=17244656, ['name']="FieldManual",             ['polutils_name']="Field Manual",                       ['type']="Simple NPC",   ['index']=496, ['level']=0,   ['x']=472.690,  ['y']=-1.165,   ['z']=-63.307,  ['r']=66,  ['flag']=23,     ['speed']=50, ['speedsub']=50, ['animation']=0,  ['animationsub']=8,  ['namevis']=32,  ['status']=0,  ['flags']=3,       ['name_prefix']=0,   ['look']="0000F208",                                 ['raw_packet']="0E220B00F0210701F0010F425258EC43B81E95BF5E3A7DC21700000032326400000300000000000000000820000000000000F2084669656C644D616E75616C0000000000",},
+    [17244667] = {['id']=17244667, ['name']="blank",                   ['polutils_name']="     ",                              ['type']="Decoration",   ['index']=507, ['level']='X', ['x']=398.651,  ['y']=16.000,   ['z']=596.820,  ['r']=89,  ['flag']=98305,  ['speed']=50, ['speedsub']=50, ['animation']=0,  ['animationsub']=0,  ['namevis']=0,   ['status']=2,  ['flags']=2075,    ['name_prefix']=32,  ['look']="00004701",                                 ['raw_packet']="0E1C0B00FB210701FB010F595453C743000080417B3415440180010032326400021B080000000020A0000000000000000000470100000000",},
+}
+return zone_database
